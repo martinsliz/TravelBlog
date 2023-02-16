45d025dae5993b45e74c3497be413bb175a0af2d
Liz Martins <elizabeth.martins5@gmail.com>
2023-02-16 11:24:54 -0500

    destinations css

--- client/src/components/UpdateReview.jsx
+++ client/src/components/UpdateReview.jsx
@@ -19,7 +19,7 @@ const handleSubmit = async (event) => {
 
   await axios.put(`${BASE_URL}/reviewRouter/review/${formState._id}`, formState)
 getAllReviews()
-setFormState('')
+setFormState({})
 }
 const deleteReview = async () => {
   const response = await axios.delete(`${BASE_URL}/reviewRouter/review/${formState._id}`)
