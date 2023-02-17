b7483b4e93bc8baef8801e9fe4d31e271dc3df71
Liz Martins <elizabeth.martins5@gmail.com>
2023-02-17 12:07:53 -0500

    project complete, previous didn't take

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,11 +2,10 @@ const About = () => {
   return (
     <div className="bucket">
       <span className="about">
-        Thanks so much for stopping in! Our goal is to create the "Wikipedia"
-        for travel destinations, where you can share your bucket-list
-        destinations with the world!. There aren't any "influencers" trying to
-        get you to buy something, just fellow travelers sharing their
-        experiences. Enjoy!
+        Our goal is to create the "Wikipedia" for travel destinations, where you
+        can share your bucket-list destinations with the world!. There aren't
+        any "influencers" trying to get you to buy something, just fellow
+        travelers sharing their experiences. Enjoy!
       </span>
     </div>
   )
