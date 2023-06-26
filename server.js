4ad5b76772c9518296cb8251e3953c7a59c6bce1
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-26 16:06:11 -0400

    switched back to port 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const routes = require('./routes')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const db = require('./db')
 
 const app = express()
