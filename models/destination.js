d9827be9f5e5b009196744172d57a47b831c3ba4
Liz Martins <elizabeth.martins5@gmail.com>
2023-08-17 17:30:59 -0400

    image field changed in model to be url

--- models/destination.js
+++ models/destination.js
@@ -8,9 +8,7 @@ const destinationSchema = new Schema(
     languages: { type: String, required: true },
     currency: { type: String, required: true },
     travelWarning: { type: String, required: true },
-    imageOne: { type: String },
-    imageTwo: { type: String },
-    imageThree: { type: String },
+    imageUrl: { type: String },
     reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
   },
   { timestamps: true }
--- models/review.js
+++ models/review.js
@@ -2,13 +2,13 @@ const { Schema } = require('mongoose')
 
 const reviewSchema = new Schema(
   {
-    rating: { type: Number, required: true },
+    rating: { type: Number },
     affordable: { type: String },
-    bestTimeToGo: { type: String, required: true },
-    whereToStay: { type: String, required: true },
-    attractions: { type: String, required: true },
-    restaurants: { type: String, required: true },
-    image: { type: String },
+    bestTimeToGo: { type: String },
+    whereToStay: { type: String },
+    attractions: { type: String },
+    restaurants: { type: String },
+    imageUrl: { type: String },
     comments: { type: String },
     destination: { type: Schema.Types.ObjectId, ref: 'Destination' }
   },
